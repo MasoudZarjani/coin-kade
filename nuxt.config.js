ce26cd0e5dc036d9da21e82af73a0b7fc99e6906
moein <moein.mousavi1378@gmail.com>
2022-09-13 21:53:55 +0430

    second commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 import fa from 'vuetify/lib/locale/fa'
 export default {
+    target: 'static',
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         titleTemplate: '%s - coin-kade',
